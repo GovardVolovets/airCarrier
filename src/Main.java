cd0cb873b26cb8c85e3a7c5c61db9b300b22e329
Govard Volovets <g3ag@yandex.ru>
2023-02-13 00:14:50 +0300

    changed src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int ticket = 144_544;
-        float condition = 20;
+        float condition = 20F;
 
         int bonusMiles = (int) (ticket / condition);
 
