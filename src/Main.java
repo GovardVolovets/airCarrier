2932442957a8ca932fea86dc33373c40f2ca0896
Govard Volovets <g3ag@yandex.ru>
2023-02-13 00:20:14 +0300

    code optimization src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int ticket = 144_544;
-        float condition = 20F;
+        byte condition = 20;
 
-        int bonusMiles = (int) (ticket / condition);
+        int bonusMiles = ticket / condition;
 
         System.out.println(bonusMiles);
     }
