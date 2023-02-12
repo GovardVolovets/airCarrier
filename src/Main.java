321bc9c72f3dcc588171d5b532e7bb132e196e2f
Govard Volovets <g3ag@yandex.ru>
2023-02-13 00:09:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 144_544;
+        float condition = 20;
+
+        int bonusMiles = (int) (ticket / condition);
+
+        System.out.println(bonusMiles);
+    }
+}
